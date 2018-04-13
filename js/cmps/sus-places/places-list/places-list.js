e53267e1d35f22434c03d31fdd39364ae099d1e4
Itay Ben Shmuel <Taytay884@yahoo.com>
2018-04-13 20:42:31 +0300

    susPlaces: added markers, places, and the functionality between them.

--- js/cmps/sus-places/places-list/places-list.js
+++ js/cmps/sus-places/places-list/places-list.js
@@ -8,24 +8,39 @@ export default {
         }
     },
     created() {
-        eventBusService.$on('placeAdded', (place) => {
-            this.places.push(place);
-        })
+        eventBusService.$on('markerClicked', markerId => {
+            placesService.selectMarker(markerId);
+        });
+        eventBusService.$on('placeAdded', (placesDB) => {
+            this.places = placesDB;
+        });
+        eventBusService.$on('mapLoaded', () => {
+            this.loadPlaces();
+        });
     },
     methods: {
-        deletePlace(index) {
-            eventBusService.$emit('placeAdded', this.selectedPlace)
-            console.log(this.selectedPlace);
+        loadPlaces() {
+            placesService.getPlaces().then((placesDB) => {
+                this.places = placesDB;
+                console.log(this.places);
+            });
+        },
+        deletePlace(id) {
+            console.log(id);
+            placesService.deletePlace(id);
+        },
+        selectPlace(id) {
+            placesService.selectPlace(id);
         }
     },
     template: `
         <section class="places-list-container">
                 <h2 class="menu-bar flex-bar">My Places:</h2>
                 <ul class="places-list clean-list">
-                    <li v-for="(place, idx) in places">
+                    <li :class="{selected: place.selected}" v-for="place in places" @click="selectPlace(place.id)">
                         <h1>{{ place.name }}</h1>
                         <h1>{{ place.description }}</h1>
-                        <button @click="deletePlace(idx)">x</button>
+                        <button @click="deletePlace(place.id)">x</button>
                         <!-- id: Date.now(),
                         name: mapData.results[0].formatted_address,
                         lat,
--- js/services/sus-places/google-maps/google-map.service.js
+++ js/services/sus-places/google-maps/google-map.service.js
@@ -6,11 +6,14 @@ import eventBusService from '../../event-bus.service.js';
 const GOOGLE_API_KEY = 'AIzaSyBLudm0GgFeS_-v7yAud09Wg4oAWJ4sLrg';
 
 var map;
-var tempMarker;
-var markers = [];
+var gTempMarker;
+var gMarkers = [];
+// google.maps.event.addListenerOnce(map, 'idle', function () {
+//     // do something only the first time the map is loaded
+// });
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-    
+
     const gmapApi = new GoogleMapsApi();
     return gmapApi.load().then(() => {
         map = new google.maps.Map(
@@ -21,11 +24,11 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
                 },
                 zoom: 15
             })
+        // Sending an event that map loaded to start render places' markers on it.
+        eventBusService.$emit('mapLoaded');
         google.maps.event.addListener(map, 'click', function (event) {
-            // console.log(event) GET THE X, Y of the screen to make the PLACE DETAILS
-            addMarker(event.latLng);
-            getLocationByPos(+event.latLng.lat(), +event.latLng.lng());
-            map.panTo({lat: +event.latLng.lat(), lng: +event.latLng.lng()})
+            let latLng = {lat: +event.latLng.lat(), lng: +event.latLng.lng()}
+            eventBusService.$emit('newPlaceClicked', latLng);
         });
         console.log('Map has been loaded.', map);
     });
@@ -37,34 +40,23 @@ function getLocation(locationName) {
             return response.json();
         })
         .then(function (mapData) {
-            map.panTo(mapData.results[0].geometry.location);
-            addMarker(mapData.results[0].geometry.location);
+            let latLng = mapData.results[0].geometry.location;
+            eventBusService.$emit('placeSearched', locationName, latLng);
             return mapData.results[0];
         })
 }
 
 function getLocationByPos(lat, lng) {
-    console.log('Getting place data...')
     return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${GOOGLE_API_KEY}`)
         .then(function (response) {
             return response.json();
         })
-        .then(function (mapData) {
-            let currPlace = {
-                id: Date.now(),
-                name: mapData.results[0].formatted_address,
-                lat,
-                lng,
-                tags: [],
-                description: '',
-                photos: []
-            }
-            eventBusService.$emit('placeClicked', currPlace)
-            console.log(mapData.results[0].formatted_address);
-            
-            // Name, Description, id, Photos(allow adding
-            // photos), lat, lng, tag(fun / food / work / anything…)
-        });
+        .then(function (placeData) {
+            // console.log(placeData.results.length)
+            if (!placeData.results.length)
+                return Error('No results!');
+            return placeData.results;
+        })
 }
 
 // 
@@ -83,26 +75,67 @@ function getAutocompleteList(locationName) {
         });
 }
 
-function setCenter (pos) {
+function setCenter(pos) {
     map.panTo(pos);
+}
+
+function addMarker(loc, id) {
+    // Deletes the previous marker if it was not saved.
+    if (gTempMarker && !gTempMarker.get('saved')) {
+        gTempMarker.setMap(null);
+    }
+    gTempMarker = new google.maps.Marker({
+        position: loc,
+        map,
+    })
+    gTempMarker.set('id', (id) ? id : Date.now());
+    gTempMarker.setIcon('../../../../img/sus-places/new-place-icon.png');
+    return gTempMarker.get('id');
+}
+
+function removeMarker(id) {
+    let marker = gMarkers.find(marker => {
+        console.log(marker);
+        console.log(marker.id);
+        return marker.id === id;
+    });
+    marker.setMap(null)
+    marker.set('saved', false);
+}
+
+function saveMarker(id) {
+    if (gTempMarker) {
+        let markerId = gTempMarker.get('id');
+        gMarkers.push(gTempMarker);
+        gTempMarker.setMap(map);
+        gTempMarker.set('saved', true);
+        gTempMarker.setIcon('../../../../img/sus-places/saved-place-icon.png');
+        gTempMarker.addListener('click', () => {
+            eventBusService.$emit('markerClicked', markerId);
+        });
+        return Promise.resolve();
+    }
+}
+
+function toggleMarker(id, toSelect) {
+    let marker = gMarkers.find((marker) => {
+        return marker.id === id;
+    });
+    if (toSelect) {
+        marker.setIcon('../../../../img/sus-places/clicked-place-icon.png');
+    } else {
+        marker.setIcon('../../../../img/sus-places/saved-place-icon.png');
+    }
 }
 
 export default {
     initMap,
     setCenter,
     getLocation,
-    getAutocompleteList
-}
-
-function addMarker(loc) {
-    if(tempMarker)
-        tempMarker.setMap(null)
-        
-    tempMarker = new google.maps.Marker({
-        position: loc,
-        map,
-    })
-    tempMarker.setIcon('https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png');
-    // markers.push(marker);
-    console.log(tempMarker);
+    getLocationByPos,
+    getAutocompleteList,
+    addMarker,
+    saveMarker,
+    removeMarker,
+    toggleMarker
 }
